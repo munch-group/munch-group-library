681456ee6c36006e866bedf7d21272a7d2ce8e2d
Kasper Munch <kaspermunch@birc.au.dk>
2026-02-20 10:11:52 +0100

    update

--- scripts/docs-build-render.sh
+++ scripts/docs-build-render.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-pip install --quiet --no-deps --force-reinstall -e . \
-    && cd docs \
-    && rm -f api/_styles-quartodoc.css api/_sidebar.yml *.qmd \
-    && quartodoc build && quartodoc interlinks && quarto render \
-    && cd .. \
-    && pip uninstall --quiet -y munch-group-library
+cd docs
+#rm -f api/_styles-quartodoc.css api/_sidebar.yml #*.qmd
+quartodoc build && quartodoc interlinks && quarto render
+cd ..
